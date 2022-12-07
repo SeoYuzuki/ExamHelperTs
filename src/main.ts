58a56dff40995d54601515fc40292096a50e42ef
SeoYuzuki <st89200@gmail.com>
2022-12-07 13:44:09 +0800

    upd

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import ViewUIPlus from "view-ui-plus";
 import App from "./App.vue";
 import "view-ui-plus/dist/styles/viewuiplus.css";
 import VueCookies from 'vue3-cookies'
-import { useCookies } from "vue3-cookies";
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 
-createApp(App).use(ViewUIPlus).use(VueCookies)
-    .mount("#app");
+const app = createApp(App);
+app.use(ViewUIPlus);
+app.use(VueCookies);
+app.use(VueAxios, axios)
+app.mount("#app");
